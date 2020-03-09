b71b7053fcf4d5aeea4459cd05affc4a82366274
Uday Yadav <dev117uday@gmail.com>
2020-03-09 20:10:57 +0530

    Create SolutionByUday.cpp
    reverse string cpp

---
+++ Basic/Reverse A String/SolutionByUday.cpp
@@ -0,0 +1,16 @@
+/*
+    Program Description program to find the gcd of two given number
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    string input_string;
+    cout << "input = ";
+    getline(cin,input_string);
+    reverse(input_string.begin(),input_string.end());
+    cout << "reverse : " << input_string << endl;
+}

16e69a93ef1e4f144b2b02daddab5befde83ba0a
Uday Yadav <dev117uday@gmail.com>
2020-03-09 18:25:17 +0530

    Create SolutionByUday.cpp
    base^power

---
+++ Basic/Implement Power Function/SolutionByUday.cpp
@@ -0,0 +1,21 @@
+/*
+    Program Description program to base^power
+    Function Name : gcd_of_two_numbers(int,int) -- return type int
+  
+*/
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    double base=0, power=0;
+    cout << "Base = ";
+    cin >> base;
+    cout << "power = ";
+    cin >> power ;
+    cout << "Output : " << pow(base,power) << endl;  
+
+}

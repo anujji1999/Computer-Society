97a0a52faf61db12b9b13380253e4313ec2e6de1
Pranav Nigam <61621016+npranavn@users.noreply.github.com>
2020-03-20 14:29:35 +0530

    SolutionByPranav.cpp

---
+++ Basic/Reverse A String/SolutionByPranav.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h> 
+
+using namespace std; 
+
+int main() 
+{ 
+    string st; 
+    cout<<"Input = ";
+    getline(cin,st);
+    reverse(st.begin(), st.end()); 
+    cout<<"Output = "<<st;
+    return 0; 
+}

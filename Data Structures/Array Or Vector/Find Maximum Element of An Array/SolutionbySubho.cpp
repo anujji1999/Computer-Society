1cb59d1186f797db9ccf5a2fe792dad775ac3165
Subhodeep2000 <61930406+Subhodeep2000@users.noreply.github.com>
2020-03-20 20:42:48 +0530

    Create SolutionbySubho.cpp

---
+++ Data Structures/Array Or Vector/Find Maximum Element of An Array/SolutionbySubho.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+int maximum(int*arr,int n){
+    int max=arr[0];
+    for(int i=1;i<n;i++)
+    {
+        if(arr[i]>max)
+        {
+            max=arr[i];
+        }
+      
+    }
+      return max;
+    
+}
+int main()
+{
+    int n;
+    cout<<"Enter the size of array"<<endl;
+    cin>>n;
+    int *arr=new int[n];
+    cout<<"Enter array elements"<<endl;
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+    cout<<"Maximum element"<<" "<<maximum(arr,n)<<endl;
+    return 0;
+}

520050af9af0e85ab34cbaf93cd611cf72f4847d
Sowmya Yellapragada <56383804+sowmya8900@users.noreply.github.com>
2020-03-20 18:11:20 +0530

    Create SolutionBySowmya.py

---
+++ Basic/Compute Length of A String/SolutionBySowmya.py
@@ -0,0 +1,7 @@
+# Defining function for calculating the length of the string.
+def len_of_string(s):
+   print(len(string))
+# taking the input from the user
+string=str(input("Enter string of any length:"))
+#function calling:
+len_of_string(string)

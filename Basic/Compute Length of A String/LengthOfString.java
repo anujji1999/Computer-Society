8c7f83d0459888490b09314663bdd965f97f8275
Sayam2801 <55996885+Sayam2801@users.noreply.github.com>
2020-03-22 15:22:52 +0530

    Create LengthOfString.java

---
+++ Basic/Compute Length of A String/LengthOfString.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class LengthOfString{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        String s;
+        s=sc.nextLine();
+        System.out.print(s.length());
+        sc.close();
+    }
+}

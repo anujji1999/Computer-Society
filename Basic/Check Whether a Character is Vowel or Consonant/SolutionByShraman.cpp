e4af8b2c48f363ac04fc0815ba582add67838cdb
Shraman Jain <52606707+ShramanJain@users.noreply.github.com>
2020-03-23 03:04:27 +0530

    Update SolutionByShraman.cpp

--- Basic/Check Whether a Character is Vowel or Consonant/SolutionByShraman.cpp
+++ Basic/Check Whether a Character is Vowel or Consonant/SolutionByShraman.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 string Vowels(char c)
 {
-    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+    if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
     {
         return "Vowel";
     }
